562ae5508ba0bce1d15ca0e9cf95edc03671f7ad
Liam <git@liamsherwin.com>
2025-12-24 20:34:05 +1300

    init

--- Client.gd
+++ Client.gd
@@ -1,6 +1,10 @@
 class_name Client extends Node
 
-func hello_utils(p_source: String) -> void:
+func _init() -> void:
+	hello_client("Client.init()")
+
+
+func hello_client(p_source: String) -> void:
 	prints(p_source, "> Hello, Client!")
 	Network.hello_network("Client")
 	Utils.hello_network("Client")
